44f8469b312e5bf95683812d5edbb533b5112e05
Guilherme-Bodart <guilhermebodart73@gmail.com>
2020-09-21 16:53:54 -0300

    Database na nuvem + porta
    Coloquei o database na nuvem agora, e mudei a porta pq o front usa 3000 tbm

--- public/src/database/index.js
+++ public/src/database/index.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
+const URI = "mongodb+srv://outrigger:laboral@cluster0.rda29.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority"
+
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost/noderest", {
+mongoose.connect(URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true
