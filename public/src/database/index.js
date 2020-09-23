0b717c3f17785a6632c8c826e8c52cad12922a71
Guilherme-Bodart <guilhermebodart73@gmail.com>
2020-09-23 16:00:15 -0300

    Banco teste + producao

--- public/src/database/index.js
+++ public/src/database/index.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
-const URIPRODUCAO = "mongodb+srv://outrigger:laboral@cluster0.rda29.gcp.mongodb.net/producao?retryWrites=true&w=majority"
-const URITESTE = "mongodb+srv://outrigger:laboral@cluster0.rda29.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority"
+const URI_PRODUCAO = "mongodb+srv://outrigger:laboral@cluster0.rda29.gcp.mongodb.net/producao?retryWrites=true&w=majority"
+const URI_TESTE = "mongodb+srv://outrigger:laboral@cluster0.rda29.gcp.mongodb.net/teste?retryWrites=true&w=majority"
 
 mongoose.Promise = global.Promise;
-mongoose.connect(URIPRODUCAO, {
+mongoose.connect(URI_PRODUCAO, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true
