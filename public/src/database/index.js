9b932b6e8e3fe123388686bbe4e714d27787058b
Guilherme-Bodart <guilhermebodart73@gmail.com>
2020-09-23 15:57:51 -0300

    URIPRODUCAO

--- public/src/database/index.js
+++ public/src/database/index.js
@@ -3,7 +3,7 @@ const URIPRODUCAO = "mongodb+srv://outrigger:laboral@cluster0.rda29.gcp.mongodb.
 const URITESTE = "mongodb+srv://outrigger:laboral@cluster0.rda29.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority"
 
 mongoose.Promise = global.Promise;
-mongoose.connect(URI, {
+mongoose.connect(URIPRODUCAO, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true
