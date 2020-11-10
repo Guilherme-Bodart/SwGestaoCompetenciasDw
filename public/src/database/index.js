26d9120b600c8bdfe686b02bbf19f93ae5a77a51
Guilherme-Bodart <guilhermebodart73@gmail.com>
2020-11-09 21:11:42 -0300

    Update index.js

--- public/src/database/index.js
+++ public/src/database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-const URI_PRODUCAO = "mongodb+srv://outrigger:laboral@cluster0.rda29.gcp.mongodb.net/producao?retryWrites=true&w=majority"
-const URI_TESTE = "mongodb+srv://outrigger:laboral@cluster0.rda29.gcp.mongodb.net/teste?retryWrites=true&w=majority"
+const URI_PRODUCAO = "mongodb+srv://outrigger:******@cluster0.rda29.gcp.mongodb.net/producao?retryWrites=true&w=majority"
+const URI_TESTE = "mongodb+srv://outrigger:******@cluster0.rda29.gcp.mongodb.net/teste?retryWrites=true&w=majority"
 
 mongoose.Promise = global.Promise;
 mongoose.connect(URI_PRODUCAO, {
